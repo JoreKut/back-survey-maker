7df142a4be293805d79bc1cd482a836241a58056
JoreKut <37114216+JoreKut@users.noreply.github.com>
2022-04-08 23:54:17 +0300

    Initial commit

---
+++ src/main/java/com/petproject/surveymaker/SurveyMakerApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.surveymaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyMakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petproject/surveymaker/SurveyMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petproject.surveymaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
